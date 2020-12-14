85ab1d32fa84d3e34648c2b896162574493640e8
Panc4kes <jack.sun4@student.education.wa.edu.au>
2020-12-14 21:50:29 +0800

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ running_script_directory = os.path.dirname(os.path.realpath(__file__))
 os.chdir(running_script_directory)
 
 for file in glob.glob("stockfish*"):
-    print("Found Stockfish binary version",file.strip(".exe"))
+    print("Found Stockfish binary version",file.strip("stockfish_").strip(".exe"))
     stockfish_name = file
 
 try:
@@ -28,20 +28,25 @@ limit = chess.engine.Limit(time=0.2)
 driver = webdriver.Chrome("chromedriver.exe")
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--no-sandbox')
+<<<<<<< Updated upstream
+=======
+chrome_options.add_argument("--log-level=3")
+driver = webdriver.Chrome("chromedriver.exe", options=chrome_options)
+>>>>>>> Stashed changes
 
 with open("board.txt") as f:
     array = [i.split() for i in f]
 
 #url = input("Enter a url\n> ")
-url = "https://www.chess.com/play/computer"
+url = "https://www.chess.com/play/computer?fen=qkb3nr/ppppppPp/8/8/8/8/PPPPPPP1/RNBQKBNR%20w%20KQ%20-%200%201"
 driver.get(url)
 input("Press any key to continue...")
 
 def open_chrome():
     '''
-    THIS FUNCTION ASSURES THAT CHROME IS OPEN SO check_fen() CAN WORK PROPERLY
+    Funtion makes sure that Chrome is open so that check_fen can work properly.
     '''
-    app = application.Application().connect(title_re =".*Chess.*")
+    app = application.Application().connect(title_re ="Play Chess.*")
     app_dialog = app.top_window()
 
     if not app_dialog.has_focus():
@@ -66,15 +71,24 @@ def find_loc(piece):
             if col == piece:
                 return [j+1, 8-i]
 
+<<<<<<< Updated upstream
 while not board.is_game_over():
     board = chess.Board(check_fen())
+=======
+initial_fen = check_fen()
+while not board.is_game_over():    
+>>>>>>> Stashed changes
     piece_size = driver.find_element_by_css_selector(".layout-board.board").size["height"]/8
-    
+    while True:
+        fen = check_fen()
+        if board.fen() != fen or board.fen() == initial_fen:
+            board = chess.Board(fen)
+            break
+
     result = engine.play(board, limit)
     origin = find_loc(str(result.move)[:2])
-    target = find_loc(str(result.move)[-2:])
+    target = find_loc(str(result.move)[2:4])
     offset = [a - b for a, b in zip(target, origin)]
-    # print(origin, target)
     offset[0] *= piece_size
     offset[1] *= -piece_size
     
@@ -92,6 +106,14 @@ while not board.is_game_over():
         promotion_button.click()
         time.sleep(2)
     board.push(result.move)
+<<<<<<< Updated upstream
+=======
+
+    if len(str(result.move)) == 5:
+        promotion = driver.find_element_by_css_selector("div.promotion-piece." + fen.split()[1] + str(result.move)[-1].lower())
+    print(board, "\n")
+
+>>>>>>> Stashed changes
 
     time.sleep(3)
     print(board, "\n")
